868182d2b0295eeaceca53b851e8e5396f01372a
visheshsharma551 <visheshsharma551@gmail.com>
2024-02-01 13:44:10 -0800

    nav bar Responsive work

--- freelancer.js
+++ freelancer.js
@@ -1,6 +1,20 @@
 let navBar = document.querySelector("nav");
 let header = document.querySelector(".header");
 
+// Hiding and displaying the Menu bar in  responsive
+
+let menuList = document.querySelector(".Menu-Link")  
+let menuImg = document.querySelector(".menu-img")
+// function ToggleEvent(){
+    // menuList.classList.toggle("show-menu");         //this is the other way to write the fuction in which onclick event is addede on HTMl
+// }
+// 
+
+menuImg.onclick =  function (){
+    menuList.classList.toggle("show-menu");
+}
+
+
 window.onscroll = function () {
   if (window.scrollY > 0) {
     navBar.style.background = "#eefff9";
@@ -8,3 +22,10 @@ window.onscroll = function () {
     navBar.style.background = "transparent";
   }
 };
+
+
+
+
+
+
+
